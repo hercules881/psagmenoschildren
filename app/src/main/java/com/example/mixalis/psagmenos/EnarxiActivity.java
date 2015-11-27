54d5980099eb18259ef8c0974ee3810a6f777f1e
h.lionakis <h_lion88@hotmail.com>
2015-11-27 21:18:07 +0200

    ...

--- app/src/main/java/com/example/mixalis/psagmenos/EnarxiActivity.java
+++ app/src/main/java/com/example/mixalis/psagmenos/EnarxiActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,7 +23,14 @@ public class EnarxiActivity extends Activity {
     ListView listView;
     String[] katigories;
 
+    private ImageButton math;
+    private ImageButton alphavita;
+    private ImageButton glwssa;
+    private ImageButton xrwmata;
+    private ImageButton zwgrafiki;
 
+    public EnarxiActivity() {
+    }
 
     @ Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,36 +39,113 @@ public class EnarxiActivity extends Activity {
         initCats();
 
 
-        CustomAdapter adapter = new CustomAdapter(this, katigories);
+        /*CustomAdapter adapter = new CustomAdapter(this, katigories);
         listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapter);*/
 
 
-
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+       /* listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Intent myIntent = new Intent(EnarxiActivity.this, GameActivity.class);
                 // EnarxiActivity.this.startActivity(myIntent);
-
-                if (position ==  5) {
+                if (position == 4) {
                     Intent myIntent = new Intent(EnarxiActivity.this, PaintActivity.class);
                     EnarxiActivity.this.startActivity(myIntent);
                 }
+                if (position != 4) {
+                    Intent i = new Intent(getApplicationContext(), GameActivity.class);
+                    // i.putExtra("epelexes","Γεωγραφία");
 
-                if (position !=  5 && position != 2){
-                Intent i = new Intent(getApplicationContext(), GameActivity.class);
-                // i.putExtra("epelexes","Γεωγραφία");
-
-                i.putExtra("firstName", katigories[position]);
-                i.putExtra("isalphabete", katigories[position].equals(EnarxiActivity.this.getString(R.string.alphabete)));
-                i.putExtra("iscolor", katigories[position].equals(EnarxiActivity.this.getString(R.string.colors)));
-                startActivity(i);
+                    i.putExtra("firstName", katigories[position]);
+                    i.putExtra("isalphabete", katigories[position].equals(EnarxiActivity.this.getString(R.string.alphabete)));
+                    i.putExtra("iscolor", katigories[position].equals(EnarxiActivity.this.getString(R.string.colors)));
+                    startActivity(i);
                 }
-        }
+            }
+        });*/
+
+
+        ////////////////////////////////////////////////////////////
+        math = (ImageButton) findViewById(R.id.math);
+        math.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(getApplicationContext(), GameActivity.class);
+                i.putExtra("firstName", katigories[0]);
+                i.putExtra("isalphabete", katigories[0].equals(EnarxiActivity.this.getString(R.string.alphabete)));
+                i.putExtra("iscolor", katigories[0].equals(EnarxiActivity.this.getString(R.string.colors)));
+                startActivity(i);
+            }
         });
-    }
+
+
+
+        glwssa = (ImageButton) findViewById(R.id.glwssa);
+        glwssa.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(getApplicationContext(), GameActivity.class);
+                i.putExtra("firstName", katigories[1]);
+                i.putExtra("isalphabete", katigories[1].equals(EnarxiActivity.this.getString(R.string.alphabete)));
+                i.putExtra("iscolor", katigories[1].equals(EnarxiActivity.this.getString(R.string.colors)));
+                startActivity(i);
+            }
+        });
+
+
+
+
+        xrwmata = (ImageButton) findViewById(R.id.xrwmata);
+        xrwmata.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(getApplicationContext(), GameActivity.class);
+                i.putExtra("firstName", katigories[2]);
+                i.putExtra("isalphabete", katigories[2].equals(EnarxiActivity.this.getString(R.string.alphabete)));
+                i.putExtra("iscolor", katigories[2].equals(EnarxiActivity.this.getString(R.string.colors)));
+                startActivity(i);
+            }
+        });
+
+
+
+
+        alphavita = (ImageButton) findViewById(R.id.alphavita);
+        alphavita.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(getApplicationContext(), GameActivity.class);
+                i.putExtra("firstName", katigories[3]);
+                i.putExtra("isalphabete", katigories[3].equals(EnarxiActivity.this.getString(R.string.alphabete)));
+                i.putExtra("iscolor", katigories[3].equals(EnarxiActivity.this.getString(R.string.colors)));
+                startActivity(i);
+            }
+        });
+
+
+
+
+        zwgrafiki = (ImageButton) findViewById(R.id.zwgrafiki);
+        zwgrafiki.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent myIntent = new Intent(EnarxiActivity.this, PaintActivity.class);
+                EnarxiActivity.this.startActivity(myIntent);
+            }
+        });
+
+
+
+
+}
+
+
 
 
 
@@ -68,14 +153,11 @@ public class EnarxiActivity extends Activity {
               katigories = new String[]{
                 EnarxiActivity.this.getString(R.string.maths),
                 EnarxiActivity.this.getString(R.string.languageCategory),
-
-                         EnarxiActivity.this.getString(R.string.forChildren),
                 EnarxiActivity.this.getString(R.string.colors),
                 EnarxiActivity.this.getString(R.string.alphabete),
                       EnarxiActivity.this.getString(R.string.paint)
 
         };
     }
-
 
 }
