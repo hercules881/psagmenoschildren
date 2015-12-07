099deceb76119f4ee2055d68cf16251ba37722fb
mixalis <mvamvolis@powercontrol.gr>
2015-12-07 14:53:28 +0200

    eftiaxa kai ti vasi

--- app/src/main/java/com/example/mixalis/psagmenos/ShapesActivity.java
+++ app/src/main/java/com/example/mixalis/psagmenos/ShapesActivity.java
@@ -3,12 +3,16 @@ package com.example.mixalis.psagmenos;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 
+import Database.Question;
 
 
 /**
@@ -19,24 +23,33 @@ public class ShapesActivity extends Activity {
     ImageButton apantisi2;
     ImageButton apantisi3;
      ImageButton apantisi4;
+    ImageView imageshape;
 
     TextView erwtisi;
+    private Handler handler = new Handler();
+    ArrayList<Integer> lastQuestionNumber = new ArrayList<>();
+    int randomNumer;
+    Thread thread;
+    ArrayList<Question> questions;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // Intent intent = getIntent();
+        Intent intent = getIntent();
         setContentView(R.layout.gameshapes);
 
-       /* String epelexes = intent.getExtras().getString("epelexes");
+        String epelexes = intent.getExtras().getString("epelexes");
         TextView title = (TextView) findViewById(R.id.title);
         title.setText(epelexes);
 
         apantisi1 = (ImageButton) findViewById(R.id.apantisi1);
         apantisi2 = (ImageButton) findViewById(R.id.apantisi2);
         apantisi3 = (ImageButton) findViewById(R.id.apantisi3);
-        apantisi4 = (ImageButton) findViewById(R.id.apantisi4);*/
+        apantisi4 = (ImageButton) findViewById(R.id.apantisi4);
+        erwtisi = (TextView) findViewById(R.id.erwtisi);
+        imageshape = (ImageView) findViewById(R.id.imageshapesCategory);
+
 
 }
 
